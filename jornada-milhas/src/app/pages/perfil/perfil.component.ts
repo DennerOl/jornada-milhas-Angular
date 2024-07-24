c1260a10a19adf137d087573685f758ebb653969
DennerOl <dener155@hotmail.com>
2024-07-23 21:32:40 -0300

    Carregando os dados para editar o perfil do user

--- jornada-milhas/src/app/pages/perfil/perfil.component.ts
+++ jornada-milhas/src/app/pages/perfil/perfil.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { PessoaUsuaria } from '../core/types/type';
+import { TokenService } from '../core/services/token.service';
+import { CadastroService } from '../core/services/cadastro.service';
+import { FormGroup } from '@angular/forms';
+import { FormularioService } from '../core/services/formulario.service';
 
 @Component({
   selector: 'app-perfil',
@@ -6,9 +11,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil.component.scss'],
 })
 export class PerfilComponent {
-  titulo = 'Olá Pessoa';
+  titulo = 'Olá ';
   textoBotao = 'Atualizar';
   perfilComponent = true;
+
+  token = '';
+  nome = '';
+  cadastro!: PessoaUsuaria;
+  form!: FormGroup<any> | null;
+
+  constructor(
+    private tokenService: TokenService,
+    private cadastroService: CadastroService,
+    private formularioService: FormularioService
+  ) {}
+
+  ngOnInit(): void {
+    this.token = this.tokenService.retornarToken();
+    this.cadastroService.buscarCadastro(this.token).subscribe((cadastro) => {
+      this.cadastro = cadastro;
+      this.nome = this.cadastro.nome;
+      this.carregarFormulario();
+    });
+  }
+
+  carregarFormulario() {
+    this.form = this.formularioService.getCadastro();
+    this.form?.patchValue({
+      nome: this.cadastro.nome,
+      nascimento: this.cadastro.nascimento,
+      cpf: this.cadastro.cpf,
+      telefone: this.cadastro.telefone,
+      email: this.cadastro.email,
+      senha: this.cadastro.senha,
+      cidade: this.cadastro.cidade,
+      estado: this.cadastro.estado,
+      genero: this.cadastro.genero,
+    });
+  }
 
   deslogar() {}
 
